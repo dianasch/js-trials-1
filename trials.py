da916e3b5f2c23d1b3e4ec041d3728f0cfa1265c
dianasch <dianaschao@gmail.com>
2020-10-27 16:29:19 +0000

    Adds get_all_evens function.
    Co-authored-by: laura-newland <laurasmora101@gmail.com>

--- trials.py
+++ trials.py
@@ -2,12 +2,28 @@
 
 
 def output_all_items(items):
+    #Loop through each item in items
     for item in items:
+
+        #Print each item
         print(item)
 
 
 def get_all_evens(nums):
-    pass  # TODO: replace this line with your code
+    # Create an empty list to store even nums
+    even_nums = []
+
+    # Loop through each number in nums
+    for num in nums:
+
+        # Determine if number is even
+        if num % 2 == 0:
+
+            # If so, append to list
+            even_nums.append(num)
+    
+    # Return list
+    return even_nums
 
 
 def get_odd_indices(items):
